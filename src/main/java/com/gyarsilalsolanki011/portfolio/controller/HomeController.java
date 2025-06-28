54f60a247a3e58ff1499fa046d1e557d8fa30895
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2025-06-28 20:35:07 +0530

    First commit

---
+++ src/main/java/com/gyarsilalsolanki011/portfolio/controller/HomeController.java
@@ -0,0 +1,39 @@
+package com.gyarsilalsolanki011.portfolio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "", "/home"})
+    public String showHomePage(Model model) {
+        model.addAttribute("title", "Home");
+        return "master";
+    }
+
+    @GetMapping("/about")
+    public String showAboutPage(Model model) {
+        model.addAttribute("title", "About");
+        return "master";
+    }
+
+    @GetMapping("/projects")
+    public String showProjectPage(Model model) {
+        model.addAttribute("title", "Projects");
+        return "master";
+    }
+
+    @GetMapping("/service")
+    public String showServicePage(Model model) {
+        model.addAttribute("title", "Service");
+        return "master";
+    }
+
+    @GetMapping("/contact")
+    public String showContactPage(Model model) {
+        model.addAttribute("title", "Contact");
+        return "master";
+    }
+}
---
+++ src/test/java/dev/gyarsilalsoolanki011/Portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gyarsilalsoolanki011.Portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
